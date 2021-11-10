c4ea04d70a266bf058e1fc1515ad789f8149ee86
yusufadell <youseefadel777@gmail.com>
2021-11-10 20:57:03 +0200

    Format Views module

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage,\
-                                  PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from .models import Post, Comment
@@ -17,7 +16,7 @@ def post_list(request, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         object_list = object_list.filter(tags__in=[tag])
 
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
@@ -27,19 +26,20 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request,
-                 'blog/post/list.html',
-                 {'page': page,
-                  'posts': posts,
-                  'tag': tag})
+    return render(
+        request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag}
+    )
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,
-                                   status='published',
-                                   publish__year=year,
-                                   publish__month=month,
-                                   publish__day=day)
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
 
     # List of active comments for this post
     comments = post.comments.filter(active=True)
@@ -61,18 +61,22 @@ def post_detail(request, year, month, day, post):
 
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
-                                  .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-                                .order_by('-same_tags','-publish')[:4]
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by(
+        '-same_tags', '-publish'
+    )[:4]
 
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post,
-                   'comments': comments,
-                   'new_comment': new_comment,
-                   'comment_form': comment_form,
-                   'similar_posts': similar_posts})
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form,
+            'similar_posts': similar_posts,
+        },
+    )
 
 
 class PostListView(ListView):
@@ -95,13 +99,15 @@ def post_share(request, post_id):
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f"{cd['name']} recommends you read {post.title}"
-            message = f"Read {post.title} at {post_url}\n\n" \
-                      f"{cd['name']}\'s comments: {cd['comments']}"
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
             send_mail(subject, message, 'admin@myblog.com', [cd['to']])
             sent = True
 
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form,
-                                                    'sent': sent})
+    return render(
+        request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent}
+    )
