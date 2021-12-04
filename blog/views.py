f10027628806062c18db4b00d8d2cee09d2341f4
yusufadell <youseefadel777@gmail.com>
2021-12-04 02:09:37 +0200

    Add card for website index so CoOoool

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def post_list(request, tag_slug=None):
         object_list = object_list.filter(tags__in=[tag])
 
     paginator = Paginator(object_list, 3)  # 3 posts in each page
-    page = request.GET.get('page')
+    page = request.GET.get("page")
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -27,7 +27,7 @@ def post_list(request, tag_slug=None):
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(
-        request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag}
+        request, "blog/post/list.html", {"page": page, "posts": posts, "tag": tag}
     )
 
 
@@ -35,7 +35,7 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         slug=post,
-        status='published',
+        status="published",
         publish__year=year,
         publish__month=month,
         publish__day=day,
@@ -46,7 +46,7 @@ def post_detail(request, year, month, day, post):
 
     new_comment = None
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # A comment was posted
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -60,38 +60,38 @@ def post_detail(request, year, month, day, post):
         comment_form = CommentForm()
 
     # List of similar posts
-    post_tags_ids = post.tags.values_list('id', flat=True)
+    post_tags_ids = post.tags.values_list("id", flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by(
-        '-same_tags', '-publish'
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
     )[:4]
 
     return render(
         request,
-        'blog/post/detail.html',
+        "blog/post/detail.html",
         {
-            'post': post,
-            'comments': comments,
-            'new_comment': new_comment,
-            'comment_form': comment_form,
-            'similar_posts': similar_posts,
+            "post": post,
+            "comments": comments,
+            "new_comment": new_comment,
+            "comment_form": comment_form,
+            "similar_posts": similar_posts,
         },
     )
 
 
 class PostListView(ListView):
     queryset = Post.published.all()
-    context_object_name = 'posts'
+    context_object_name = "posts"
     paginate_by = 3
-    template_name = 'blog/post/list.html'
+    template_name = "blog/post/list.html"
 
 
 def post_share(request, post_id):
     # Retrieve post by id
-    post = get_object_or_404(Post, id=post_id, status='published')
+    post = get_object_or_404(Post, id=post_id, status="published")
     sent = False
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
@@ -101,13 +101,13 @@ def post_share(request, post_id):
             subject = f"{cd['name']} recommends you read {post.title}"
             message = (
                 f"Read {post.title} at {post_url}\n\n"
-                f"{cd['name']}\'s comments: {cd['comments']}"
+                f"{cd['name']}'s comments: {cd['comments']}"
             )
-            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            send_mail(subject, message, "admin@myblog.com", [cd["to"]])
             sent = True
 
     else:
         form = EmailPostForm()
     return render(
-        request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent}
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
     )
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,14 +5,19 @@ from django.contrib.sitemaps.views import sitemap
 from django.contrib import admin
 from django.urls import path, include
 
-sitemaps = {'posts': PostSitemap}
+from . import views
+
+
+sitemaps = {"posts": PostSitemap}
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path("admin/", admin.site.urls),
+    # cards in website index
+    path("", views.index),
+    path("blog/", include("blog.urls", namespace="blog")),
     path(
-        'sitemap.xml',
+        "sitemap.xml",
         sitemap,
-        {'sitemaps': sitemaps},
-        name='django.contrib.sitemaps.views.sitemap',
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
     ),
 ]
