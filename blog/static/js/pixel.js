b6c565f746275b024b369fe12299f07f233c4151
yusufadell <youseefadel777@gmail.com>
2022-01-15 06:04:13 +0200

    Add Assests new blog post page

---
+++ blog/static/js/pixel.js
@@ -0,0 +1,28 @@
+const d = document;
+d.addEventListener("DOMContentLoaded", function (event) {
+
+
+    // update target element content to match number of characters
+    var dataBindCharacters = [].slice.call(document.querySelectorAll('[data-bind-characters-target]'))
+    dataBindCharacters.map(function (el) {
+        var text = d.querySelector(el.getAttribute('data-bind-characters-target'));
+        var maxCharacters = parseInt(el.getAttribute('maxlength'));
+        text.textContent = maxCharacters;
+
+        el.addEventListener('keyup', function (event) {
+            var string = this.value;
+            var characters = string.length;
+            var charactersRemaining = maxCharacters - characters;
+            text.textContent = charactersRemaining;
+        });
+
+        el.addEventListener('change', function (event) {
+            var string = this.value;
+            var characters = string.length;
+            var charactersRemaining = maxCharacters - characters;
+            text.textContent = charactersRemaining;
+        });
+
+    });
+
+});
