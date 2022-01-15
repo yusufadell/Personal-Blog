f534dfeee9a274d6e189f3da207d9f73dca15601
yusufadell <youseefadel777@gmail.com>
2022-01-15 06:03:30 +0200

    Add attributes to style form fields

--- blog/forms.py
+++ blog/forms.py
@@ -8,8 +8,20 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
+    widgets = {
+        'comments': forms.Textarea(attrs={'placeholder': 'add yout comment', 'class': 'form-control border border-gray-300-gray', 'data-bind-characters-target': '#charactersRemaining', 'maxlength': '1000'}),
+        'name': forms.TextInput(attrs={'class': 'form-control border border-gray-300-gray'}),
+        'email': forms.TextInput(attrs={'class': 'form-control border border-gray-300-gray'}),
+        'to': forms.TextInput(attrs={'class': 'form-control border border-gray-300-gray'}),
+    }
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'email', 'body')
+        widgets = {
+            'body': forms.Textarea(attrs={'placeholder': 'add yout comment', 'class': 'form-control border border-gray-300-gray', 'data-bind-characters-target': '#charactersRemaining', 'maxlength': '1000'}),
+            'name': forms.TextInput(attrs={'class': 'form-control border border-gray-300-gray'}),
+            'email': forms.TextInput(attrs={'class': 'form-control border border-gray-300-gray'})
+        }
